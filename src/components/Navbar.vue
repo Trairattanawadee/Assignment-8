5f2d2900f818317b9885e8aa6176e8ee512ad2dd
Trairattanawadee <112239330+Trairattanawadee@users.noreply.github.com>
2022-10-16 19:43:57 +0700

    Add files via upload

---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <nav class="navbar">
+    <a href="/" class="active"> <i class="fas fa-user"></i> <span>Home</span> </a>
+    <a href="abilities"> <i class="fas fa-address-card"></i> <span>About Us</span> </a>
+    <a href="experience"> <i class="fas fa-address-card"></i> <span>Booth</span> </a>
+    <a href="projects"> <i class="fas fa-briefcase"></i> <span></span>Work</a>
+    <a href="contact"> <i class="fas fa-address-book"></i> <span>Contact</span> </a>
+</nav>
+    </div>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
